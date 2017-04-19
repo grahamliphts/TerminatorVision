9ade4954af4e5389883c296d227ebff886db499f
Guillaume Stienon <tk_368@hotmail.com>
2017-04-19 13:06:20 +0200

    GraphCreation Stepping

--- Terminator/graphCreation.h
+++ Terminator/graphCreation.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <opencv2/imgproc.hpp>
 
-cv::Mat GetGraph(cv::Mat img);
-std::vector<cv::Mat> GetGraphSplitChannels(cv::Mat img);
+cv::Mat GetGraph(cv::Mat img, int step = 8);
+std::vector<cv::Mat> GetGraphSplitChannels(cv::Mat img, int step = 8);
 
 cv::Mat RedPicture(cv::Mat img);
 
-cv::Mat GetGraphTesting(cv::Mat img);
+cv::Mat GetGraphTesting(cv::Mat img, int step = 8);
